c32ca77d2fd38f1b487600c302ff7fe782b6b558
Harry Wang <zuhanwang@outlook.com>
2025-08-17 21:46:18 -0400

    updated testbench code

--- Testbench/hdl/testbench1.sv
+++ Testbench/hdl/testbench1.sv
@@ -27,13 +27,13 @@
 
 //Step 2 - Import two required packages: axi_vip_pkg and <component_name>_pkg.
 import axi_vip_pkg::*;
-import Graphics_Testing_Design_axi_vip_0_0_pkg ::*;
-import Graphics_Testing_Design_axi_vip_1_0_pkg ::*;
+import Graphics_Testing_Design_axi_vip_0_1_pkg ::*;
+import Graphics_Testing_Design_axi_vip_1_1_pkg ::*;
 //////////////////////////////////////////////////////////////////////////////////
 // Test Bench Signals
 //////////////////////////////////////////////////////////////////////////////////
 // Clock and Reset
-bit aclk = 0, aresetn = 1;
+bit aclk = 0, aclk1 = 0, aresetn = 1;
 //Simulation output
 bit led_1, switch_1;
 //AXI4-Lite signals
@@ -46,11 +46,13 @@ Graphics_Testing_Design_wrapper UUT
 (
     .aclk_0               (aclk),
     .aresetn_0            (aresetn),
-    .m00_axi_init_axi_txn_0           (switch_1)
+    .m00_axi_init_axi_txn_0           (switch_1),
+    .m00_axi_aclk_0 (aclk1)
 );
 
 // Generate the clock : 50 MHz    
-always #10ns aclk = ~aclk;
+always #10ns aclk = ~aclk; 
+always #7ns aclk1 = ~aclk1;
 
 //////////////////////////////////////////////////////////////////////////////////
 // Main Process
@@ -70,15 +72,15 @@ end
 //////////////////////////////////////////////////////////////////////////////////
 //
 // Step 3 - Declare the agent for the master VIP
-Graphics_Testing_Design_axi_vip_1_0_mst_t     master_agent;
-Graphics_Testing_Design_axi_vip_0_0_slv_mem_t     slave_agent;
-//Graphics_Testing_Design_axi_vip_0_0_slv_t     slave_agent;
+Graphics_Testing_Design_axi_vip_0_1_mst_t     master_agent;
+Graphics_Testing_Design_axi_vip_1_1_slv_mem_t     slave_agent;
+//Graphics_Testing_Design_axi_vip_1_1_slv_t     slave_agent;
 //
 initial begin    
 
     // Step 4 - Create a new agent
-    master_agent = new("master vip agent",UUT.Graphics_Testing_Design_i.axi_vip_1.inst.IF);
-    slave_agent = new("slave vip agent",UUT.Graphics_Testing_Design_i.axi_vip_0.inst.IF);
+    master_agent = new("master vip agent",UUT.Graphics_Testing_Design_i.axi_vip_0.inst.IF);
+    slave_agent = new("slave vip agent",UUT.Graphics_Testing_Design_i.axi_vip_1.inst.IF);
     // Step 5 - Start the agent
     master_agent.start_master();
     slave_agent.start_slave();
@@ -87,11 +89,21 @@ initial begin
     wait (aresetn == 1'b1);
 	#200ns
 	
+    #500ns
+    addr = 0;
+    data = 0;
+    master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
+    
+        #500ns
+    addr = 4;
+    data = 0;
+    master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
+    	
     
 //    Send 0x1 to the AXI GPIO Data register 1
     #500ns
-    addr = 0;
-    data = 1;
+    addr = 8;
+    data = 10;
     master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
     
     
@@ -119,25 +131,26 @@ initial begin
 //    switch_state = data&1'h1;
     #200ns
     addr = 16;
-    data = 32'h10000000;
+    data = 32'h7000000;
     master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
     switch_state = data&1'h1;
     
 	#200ns
     addr = 12;
-    data = 5 + (1 << 9);
+    data = 20 + (1 << 17);
     master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
     switch_state = data&1'h1;
     
 	#200ns
     addr = 12;
-    data = data + (1<<8);
+    data = data + (1<<16);
     master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
     switch_state = data&1'h1;
     
-    #200ns
+//    #200ns
+    #50us
     addr = 12;
-    data = 5 + (1 << 8);
+    data = 20 + (1 << 17);
     master_agent.AXI4LITE_WRITE_BURST(base_addr + addr,0,data,resp);
     switch_state = data&1'h1;
     
