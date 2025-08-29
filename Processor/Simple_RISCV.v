b6c72ed59cb61c2f1cbf21b248d385b8fa20cd58
Harry Wang <zuhanwang@outlook.com>
2025-08-29 15:17:21 -0400

    added instruction slicing

--- Processor/Simple_RISCV.v
+++ Processor/Simple_RISCV.v
@@ -35,19 +35,34 @@ module control_unit(
 	localparam  = R_TYPE = 7'b0110011,
 				I_TYPE_1 = 7'b0000011,
 				I_TYPE_2 = 7'b0010011,
+				JALR = 7'b1100111,
 				S_TYPE = 7'b0100011,
 				B_TYPE = 7'b1100111,
 				U_TYPE = 7'b0110111,
 				J_TYPE = 7'b1101111;
+
+	//I type
+	wire [11:0] imm_i = in_instruction[31:20];
+	//S type
+	wire [11:0] imm_s1 = {in_instruction[31:25], in_instruction[11:7]};
+	//B type
+	wire [12:0] imm_b1 = {in_instruction[31], in_instruction[7], in_instruction[30:25], in_instruction[11:8], 1'b0};
+	//U type
+	wire [9:0] imm_u = {in_instruction[31:12]};
+	// R-type
+	wire [6:0] func7 = in_instruction[31:25];
+	wire [4:0] rs2 = in_instruction[24:20];
+	wire [4:0] rs1 = in_instruction[19:15];
+	wire [6:0] func3 = in_instruction[14:12];
+	wire [4:0] rd = in_instruction[11:7];
+
 	wire [6:0] opcode = in_instruction[6:0];
+
 	always @(posedge clk) begin
 		case(opcode)
 			R_TYPE:
 			I_TYPE_1,
 			I_TYPE_2:
-			begin
-				reg_write <= 1'b1;
-			end
 			S_TYPE:
 			B_TYPE:	
 			U_TYPE:
@@ -84,6 +99,8 @@ module register_file(
 	output wire [31:0] out_read_data_1,
 	output wire [31:0] out_read_data_2	
 );
+
+	reg [31:0] reg_array [31:0];
 
 endmodule 
 
