ee74ff597195857bb1677d3ee2835f6f6d7e890c
Harry Wang <zuhanwang@outlook.com>
2025-08-30 22:10:32 -0400

    implemented R and I type instructions for control unit

--- Processor/Simple_RISCV.v
+++ Processor/Simple_RISCV.v
@@ -30,16 +30,17 @@ module control_unit(
 	output reg mem_read,
 	output reg mem_write,
 	output reg branch,
-	output reg [3:0] alu_op,
+	output reg [3:0] alu_ctrl,
+	output reg [2:0] mem_info, //000: byte, 001: halfword, 010: word, 100: unsigned byte, 101: unsigned halfword
 );
 	localparam  = R_TYPE = 7'b0110011,
-				I_TYPE_1 = 7'b0000011,
-				I_TYPE_2 = 7'b0010011,
-				JALR = 7'b1100111,
+				I_TYPE_1 = 7'b0010011,
+				I_TYPE_2 = 7'b0000011,
 				S_TYPE = 7'b0100011,
 				B_TYPE = 7'b1100111,
 				U_TYPE = 7'b0110111,
-				J_TYPE = 7'b1101111;
+				J_TYPE = 7'b1101111,
+				JALR = 7'b1100111;
 
 	//I type
 	wire [11:0] imm_i = in_instruction[31:20];
@@ -61,12 +62,62 @@ module control_unit(
 	always @(posedge clk) begin
 		case(opcode)
 			R_TYPE:
+				begin
+					reg_write <= 1'b1;
+					mem_read <= 1'b0;
+					mem_write <= 1'b0;
+					branch <= 1'b0;
+					case(func3)
+						3'b000: alu_ctrl <= (func7 == 7'b0000000) ? 4'b0010 : 4'b0110; // 0 -> add & sub
+						3'b100: alu_ctrl <= 4'b0011; // 4 -> xor
+						3'b110: alu_ctrl <= 4'b0001; // 6 -> or
+						3'b111: alu_ctrl <= 4'b0000; // 7 -> and
+						3'b001: alu_ctrl <= 4'b1000; // 1 -> sll
+						3'b101: alu_ctrl <= (func7 == 7'b0000000) ? 4'b1001 : 4'b1011; // 5 -> srl & srla
+						3'b010: alu_ctrl <= 4'b0111; // 2 -> slt
+						3'b011: alu_ctrl <= 4'b0111; // 3 -> sltu
+						default: alu_ctrl <= 4'b1111; // 0 -> NOP or undefined
+					endcase
+				end
 			I_TYPE_1,
+				begin
+					reg_write <= 1'b1;
+					mem_read <= 1'b1;
+					mem_write <= 1'b0;
+					branch <= 1'b0;
+					case(func3)
+						3'b000: alu_ctrl <= 4'b0010; // 0 -> add
+						3'b100: alu_ctrl <= 4'b0011; // 4 -> xor
+						3'b110: alu_ctrl <= 4'b0001; // 6 -> or
+						3'b111: alu_ctrl <= 4'b0000; // 7 -> and
+						3'b001: alu_ctrl <= 4'b1000; // 1 -> sll
+						3'b101: alu_ctrl <= (func7 == 7'b0000000) ? 4'b1001 : 4'b1011; // 5 -> srl & srla
+						3'b010: alu_ctrl <= 4'b0111; // 2 -> slt
+						3'b011: alu_ctrl <= 4'b0111; // 3 -> sltu
+						default: alu_ctrl <= 4'b1111; // 0 -> NOP or undefined
+					endcase
+				end
 			I_TYPE_2:
+				begin
+					reg_write <= 1'b1;
+					mem_read <= 1'b1;
+					mem_write <= 1'b0;
+					branch <= 1'b0;
+					alu_ctrl <= 4'b0010;
+					case(func3)
+						3'b000: mem_info <= 3'b000; // 0 -> load byte
+						3'b001: mem_info <= 3'b001; // 1 -> load halfword
+						3'b010: mem_info <= 3'b010; // 2 -> load word
+						3'b100: mem_info <= 3'b100; // 4 -> load unsigned byte
+						3'b101: mem_info <= 3'b101; // 5 -> load unsigned halfword
+						default: mem_info <= 3'b101; // NOP or undefined
+					endcase
+				end
 			S_TYPE:
 			B_TYPE:	
 			U_TYPE:
 			J_TYPE:
+			JALR:
 			default:
 		endcase
 	end
@@ -130,7 +181,7 @@ endmodule
 
 module alu_control(
 	input wire [3:0] in_instruction,
-	input wire [3:0] in_alu_op,
+	input wire [3:0] in_alu_ctrl,
 	output wire [3:0] out_alu_ctrl
 );
 endmodule
